952d9d5f84352e937663d9ed920f15ef98994e11
Conner Johnson <Conner.johnson33@gmail.com>
2017-04-17 16:01:53 -0400

    pokemon list complete

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
-
 # Load the file to connect to the DB
 require_relative 'db/connection'
 
 require_relative 'models/pokemon'
 # require_relative 'models/trainer'
+
+get '/' do
+  @pokemons = Pokemon.all
+  erb :"pokemons/index"
+end
+
+get '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/show"
+end
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS pokemons;
 
 CREATE TABLE pokemons(
+  id SERIAL PRIMARY KEY,
   name VARCHAR,
   poke_type VARCHAR,
   cp INTEGER,
